ea15cdc7d6019b4b90cf7850d2819fb804c4f70e
homusys <gitchewycide@gmail.com>
2026-07-16 01:18:26 +0800

    Initialize login page structure and an input class for all login operations

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -5,6 +5,47 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar());
+    return Scaffold(
+      appBar: AppBar(actions: [
+        
+      ],),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          spacing: 5.0,
+          children: [
+            Text('Login'),
+            LoginField(labelText: 'Username', onLogin: (placeholder) => {}),
+            LoginField(labelText: 'Password', onLogin: (placeholder) => {}),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginField extends StatelessWidget {
+  const LoginField({
+    super.key,
+    this.labelText = 'Placeholder',
+    required this.onLogin,
+    this.maxLength = 255,
+  });
+
+  final void Function(String) onLogin;
+  final String labelText;
+  final int maxLength;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      maxLength: maxLength,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+        ),
+        labelText: labelText,
+      ),
+    );
   }
 }

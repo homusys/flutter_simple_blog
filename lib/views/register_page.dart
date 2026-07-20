7a7b690e2374591de51029622d6ec1420d26409b
homusys <gitchewycide@gmail.com>
2026-07-20 14:54:33 +0800

    Add consumer to register page to listen for notifications

--- lib/views/register_page.dart
+++ lib/views/register_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_simple_blog/controllers/users_controller.dart';
 import 'package:flutter_simple_blog/widgets/email_field.dart';
 import 'package:flutter_simple_blog/widgets/password_field.dart';
+import 'package:provider/provider.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
@@ -40,25 +41,29 @@ class _RegisterFormState extends State<RegisterForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            EmailField(controller: _emailController),
-            PasswordField(controller: _passwordController),
-            TextButton(
-              onPressed: () => {
-                UsersController.registerUser(
-                  _formKey,
-                  _emailController.text.trim(),
-                  _passwordController.text.trim(),
-                ).then((success) => print('Success $success')),
-              },
-              child: Text('Sign Up'),
-            ),
-          ],
+    return Consumer<UsersController>(
+      builder: (context, value, child) => Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              EmailField(controller: _emailController),
+              PasswordField(controller: _passwordController),
+              TextButton(
+                onPressed: () => {
+                  value
+                      .registerUser(
+                        _formKey,
+                        _emailController.text.trim(),
+                        _passwordController.text.trim(),
+                      )
+                      .then((success) => print('Success $success')),
+                },
+                child: Text('Sign Up'),
+              ),
+            ],
+          ),
         ),
       ),
     );

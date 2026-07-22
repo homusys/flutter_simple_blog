a13403b14ba0e5886a26ac78dd64690078744b06
homusys <gitchewycide@gmail.com>
2026-07-23 02:15:24 +0800

    Create function to upload a single image to the supabase bucket

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -1,11 +1,19 @@
 import 'dart:io';
 
+import 'package:file_picker/file_picker.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class StorageService {
   static const String postImageBucket = 'post_images';
 
   SupabaseStorageClient get supaStorage => Supabase.instance.client.storage;
+
+  Future<String> uploadImageToBucket(PlatformFile image) async {
+    final fileName = '${DateTime.now().millisecondsSinceEpoch}_${image.name}';
+
+    await supaStorage.from('post_images').uploadBinary(fileName, image.bytes!);
+    return supaStorage.from('post_images').getPublicUrl(fileName);
+  }
 
   Future<List<String>> uploadImagesToBucket(
     List<File> images,
